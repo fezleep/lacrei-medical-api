d04142bb2b2cefa41662a249c1aa5eb00936a5b0
fezleep <virginio.9001@gmail.com>
2026-02-24 18:16:24 -0300

    Atualizações sobre o projeto da Lacrei.

--- apps/professionals/serializers.py
+++ apps/professionals/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Professional # Importa do Models, não dele mesmo!
+from django.utils import timezone
+
 
 class ProfessionalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professional
         fields = "__all__"
+
+    def validate_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("A data da consulta não pode ser no passado.")
+        return value
--- config/urls.py
+++ config/urls.py
@@ -1,34 +1,33 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import (
+    SpectacularAPIView, 
+    SpectacularSwaggerView, 
+    SpectacularRedocView
+)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Suas rotas da API (com o prefixo apps. que padronizamos)
+    # Rotas da API
     path('api/', include('apps.professionals.urls')), 
     path('api/', include('apps.appointments.urls')),
     
-    # Rotas de Autenticação JWT
+    # Autenticação JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Gera o arquivo esquema (JSON/YAML)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Interface Visual Swagger
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Interface Visual Redoc
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
